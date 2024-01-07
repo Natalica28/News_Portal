88761dc686e5b06666c2d6cb40c76de58274f103
Natalica28 <134252738+Natalica28@users.noreply.github.com>
2024-01-07 15:32:10 +0700

    Add files via upload

---
+++ NewsPaper/NewsPaper/celery.py
@@ -0,0 +1,18 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
+
+app = Celery('NewsPaper')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'send-weekly-newsletter': {
+        'task': 'NewsPaper.tasks.send_weekly_article_list',
+        'schedule': crontab(day_of_week='mon', hour='8'),
+        'options': {'queue': 'weekly-news'},
+    },
+}
